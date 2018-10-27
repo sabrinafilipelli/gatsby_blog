1acca313b4960bbd0f62ba7bab8997c3d1f5a403
SabrinaGear <sabrinagear.ft@gmail.com>
2018-10-26 22:18:54 -0500

    initial commit

---
+++ src/components/menu.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Menu = () => (
+  <div
+    style={{
+      background: '#f4f4f4',
+      paddingTop: '10px',
+    }}
+  >
+    <ul
+      style={{
+        listStyle: 'none',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/BlogPage">Blog</Link>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Menu
---
+++ src/pages/2018-10-26/index.md
@@ -0,0 +1,10 @@
+---
+
+path: "/post-one"
+date: "2018-04-09"
+title: "My First Gatsby Post"
+author: "Sabrina Gear"
+
+---
+
+This is my very first blog post in Gatsby
